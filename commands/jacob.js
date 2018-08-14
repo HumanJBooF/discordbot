4dc5cc1c1f9036c78c73dd2cb2ac234622c873d6
HumanJBooF <humanjboof@gmail.com>
2018-08-13 21:16:16 -0400

    2 more commands

---
+++ commands/jacob.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports.run = async = (client, message, args) => {
+    message.channel.send('what the fuckkkkk jakee')
+}
+
+module.exports.help = {
+    name: 'jake'
+}
